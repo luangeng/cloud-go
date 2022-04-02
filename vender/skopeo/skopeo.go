2830addf22750c9e2ca4dd99dcceb1cb734b70fc
luangeng <1057935399@qq.com>
2022-04-02 17:15:14 +0800

    mod

---
+++ vender/skopeo/skopeo.go
@@ -0,0 +1,17 @@
+package skopeo
+
+func UploadImage() error {
+	return nil
+}
+
+func ImageTag(name string) ([]string, error) {
+	return nil, nil
+}
+
+func DeleteImage() error {
+	return nil
+}
+
+func ListImage() ([]string, error) {
+	return nil, nil
+}
